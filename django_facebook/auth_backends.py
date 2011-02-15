5bda3015eff07fe83185ad42ba19f68d1171d85d
Riccardo Magliocchetti <riccardo.magliocchetti@gmail.com>
2011-02-15 10:49:55 +0100

    Rework auth backend
    Simplify it greatly requiring both facebook id and facebook email
    to be available and correct. While at it make it working :)

--- django_facebook/auth_backends.py
+++ django_facebook/auth_backends.py
@@ -1,41 +1,17 @@
-from django.contrib.auth import models, backends
+from django.contrib.auth import backends
 from django.contrib.contenttypes.models import ContentType
 from django.conf import settings
-#from user import models as models_user
-from django.db.models.query_utils import Q
-
 
 class FacebookBackend(backends.ModelBackend):
-    def authenticate(self, facebook_id=None, facebook_email=None):
+    def authenticate(self, facebook_id, facebook_email):
         '''
-        Authenticate the facebook user by id OR facebook_email
+        Authenticate the facebook user by id AND facebook_email
         '''
-        filter_clause = False
-        if facebook_id:
-            filter_clause = Q(facebook_id=facebook_id)
-
-        if facebook_email:
-            email_filter = Q(user__email=facebook_email)
-            if filter_clause:
-                filter_clause |= email_filter
-            else:
-                filter_clause = email_filter
-
-        if filter_clause:
-            try:
-                profile_string = settings.AUTH_PROFILE_MODULE
-            except AttributeError:
-                profile_string = None
-            if profile_string:
-                profile_model = profile_string.split('.')[-1]
-                profile_class = ContentType.objects.get(model=profile_model.lower())
-                profile = profile_class.get_object_for_this_type(filter_clause)
-                #profiles = profile_class.objects.filter(filter_clause).order_by('user')[:1]
-                if profile:
-                    user = profile.user
-                    return user
-            else:
-                raise KeyError
-
-
-
+        try:
+            profile_string = settings.AUTH_PROFILE_MODULE
+            profile_model = profile_string.split('.')
+            profile_class = ContentType.objects.get(app_label=profile_model[0].lower(), model=profile_model[1].lower())
+            profile = profile_class.get_object_for_this_type(facebook_id=facebook_id, user__email=facebook_email)
+            return profile.user
+        except:
+            return None
