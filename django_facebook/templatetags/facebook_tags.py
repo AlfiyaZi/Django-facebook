9c40b14dd6843cf734bcfae9acdfabe52ca53872
Stavros Korokithakis <stavros@stochastictechnologies.com>
2011-03-31 14:50:43 +0100

    Fix typo.

--- django_facebook/templatetags/facebook_tags.py
+++ django_facebook/templatetags/facebook_tags.py
@@ -7,7 +7,7 @@ register = template.Library()
 
 @register.simple_tag
 def load_facebook_sdk():
-    return render_to_string("djang_facebook/load_sdk.html", {"FACEBOOK_APP_ID": settings.FACEBOOK_APP_ID})
+    return render_to_string("django_facebook/load_sdk.html", {"FACEBOOK_APP_ID": settings.FACEBOOK_APP_ID})
 
 
 @register.simple_tag
