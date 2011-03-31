9ea21449abb87a8dd0c9a843ba690643a90fbe3c
Stavros Korokithakis <stavros@stochastictechnologies.com>
2011-03-31 14:40:42 +0100

    Fixed paths from the merge.

--- django_facebook/templatetags/facebook_tags.py
+++ django_facebook/templatetags/facebook_tags.py
@@ -7,9 +7,9 @@ register = template.Library()
 
 @register.simple_tag
 def load_facebook_sdk():
-    return render_to_string("facebook/load_sdk.html", {"FACEBOOK_APP_ID": settings.FACEBOOK_APP_ID})
+    return render_to_string("djang_facebook/load_sdk.html", {"FACEBOOK_APP_ID": settings.FACEBOOK_APP_ID})
 
 
 @register.simple_tag
 def show_connect_button():
-    return render_to_string("facebook/show_connect_button.html")
+    return render_to_string("django_facebook/show_connect_button.html")
